1643887fea7b47d33d961fdffcb84837139589e0
Freeda Moore <freedamoore@outlook.com>
2020-04-08 11:58:44 +1000

    created the sign in form components

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sign-in-and-sign-up.scss';
+import SignIn from '../../components/sign-in/sign-in';
+
+const SignInAndSignUpPage = () => (
+    <div>
+        <SignIn />
+        
+    </div>
+);
+
+export default SignInAndSignUpPage;
