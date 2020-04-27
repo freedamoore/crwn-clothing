d7716953a2d1eed11cee9447772d6552ea9cd070
Freeda Moore <freedamoore@outlook.com>
2020-04-28 09:28:31 +1000

    created the collection page and adding routing to the shop page in order to navigate to each collection accordingly

---
+++ src/pages/collection/collection.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import CollectionItem from '../../components/collection-item/collection-item';
+import { selectCollection } from '../../redux/shop/shop.selectors';
+import './collection.scss';
+
+
+
+const CollectionPage = ({collection}) => {
+    console.log("Logging collection", collection);
+    return(
+    <div className='collection-page'>
+        <h2>collection PAGE</h2>
+    </div>
+);
+};
+
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
