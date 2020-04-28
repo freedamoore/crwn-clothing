117392607b97c027e12f6e4b681aede578267a6a
Freeda Moore <freedamoore@outlook.com>
2020-04-29 09:12:32 +1000

    created new redux flow for directory

--- src/pages/collection/collection.jsx
+++ src/pages/collection/collection.jsx
@@ -7,10 +7,17 @@ import './collection.scss';
 
 
 const CollectionPage = ({collection}) => {
-    console.log("Logging collection", collection);
+    const { title, items } = collection;
     return(
     <div className='collection-page'>
-        <h2>collection PAGE</h2>
+        <h2 className='title'>{title}</h2>
+        <div className='items'>
+            {
+                items.map(item => (
+                    <CollectionItem key={item.id} item={item} />
+                ))
+            }
+        </div>
     </div>
 );
 };
