4e028a904ed5289ca4220590cff211b801a6f075
Freeda Moore <freedamoore@outlook.com>
2020-04-30 09:11:03 +1000

    added stripe intergration and updated favicon and title

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item';
+import StrikeCheckoutButton from '../../components/stripe-button/stripe-button';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button';
 
 const CheckoutPage = ({cartItems, total}) => (
     <div>
@@ -31,6 +33,12 @@ const CheckoutPage = ({cartItems, total}) => (
             <div className="total">
                 <span>TOTAL: ${total}</span>
             </div>
+            <div className='test-warning'>
+                *Please use the following test credit cart for payments*
+                <br />
+                4242 4242 4242 4242 - Exp: 01/24 - CVV: 123
+            </div>
+            <StripeCheckoutButton price={total} />
         </div>
     </div>
 );
