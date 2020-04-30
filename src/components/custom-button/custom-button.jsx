367a060531e3dbb8d329543bb21f0caa65aea05a
Freeda Moore <freedamoore@outlook.com>
2020-05-01 08:43:47 +1000

    introduced styled components to the homepage, header and custom-button

--- src/components/custom-button/custom-button.jsx
+++ src/components/custom-button/custom-button.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import './custom-button.scss';
+import { CustomButtonContainer } from './custom-button.styles';
 
+
+const CustomButton = ({children, ...props}) => (
+    <CustomButtonContainer {...props}>
+        {children}
+    </CustomButtonContainer>
+);
+
+export default CustomButton;
+
+/*
+** before styled components:
 const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
-    <button className={
+<button className={
         `
             ${inverted ? 'inverted': '' }
             ${isGoogleSignIn ? 'google-sign-in': '' } 
             custom-button
         `} {...otherProps}>
         {children}
-    </button>
-    
-);
-
-export default CustomButton;
+    </button> 
+    );*/
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,41 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './header.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 import CartIcon from '../cart-icon/cart-icon';
-import CartDropdown from '../cart-dropdown/cart-dropdown';
 import CartDropown from '../cart-dropdown/cart-dropdown';
 import {createStructuredSelector} from 'reselect';
 import {selectCartHidden} from '../../redux/cart/cart.selectors';
 import {selectCurrentUser} from '../../redux/user/user.selector';
+import {HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink} from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
-    <div className='header'>
-        <Link className='logo-container' to='/'>
+    <HeaderContainer>
+        <LogoContainer to='/'>
             <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/shop'>
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to='/shop'>
                 SHOP
-            </Link>
-            <Link className='option' to='/contact'>
+            </OptionLink>
+            <OptionLink to='/contact'>
                 CONTACT
-            </Link>
+            </OptionLink>
             {
                 currentUser ?
-                (<div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>)
+                (<OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>)
                 :
-                (<Link className='option' to='/signin'>SIGN IN</Link>)
+                (<OptionLink to='/signin'>SIGN IN</OptionLink>)
             }
             <CartIcon />
-        </div>
+        </OptionsContainer>
         {
             hidden ? null : <CartDropown />
         }
         
-    </div>
+    </HeaderContainer>
 );
 
 // const mapStateToProps = state => ({
